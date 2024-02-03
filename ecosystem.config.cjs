569005934a016941bccc31a6b845852fcc1c2368
Ruben Kaiser <Ruben2412000@gmail.com>
2024-02-03 12:02:45 +0100

    Added Port

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,6 +1,9 @@
 module.exports = {
     apps : [{
         name   : "IMA Frontend",
-        script : "./.output/server/index.mjs"
+        script : "./.output/server/index.mjs",
+        env: {
+            PORT: 3100
+        },
     }]
 }
